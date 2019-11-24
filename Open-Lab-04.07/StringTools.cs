89f950b0ca3fd4d67bb9aec43f6daf50ff231c5d
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-24 20:41:31 +0100

    sd

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._07
 {
     public class StringTools
     {
+ 
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            string a = "";
+            for (int b = original.Length - 1; b >= 0; b--)
+            {
+                a = a + original[b];
+            }
+            return a;
         }
     }
 }
